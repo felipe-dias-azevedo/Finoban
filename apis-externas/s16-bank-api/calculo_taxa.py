a69b29d6b482bd89342fc0f1064f097eceacd891
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2021-03-29 10:46:45 -0300

    aprimorado calculo taxa s16 bank

--- apis-externas/s16-bank-api/calculo_taxa.py
+++ apis-externas/s16-bank-api/calculo_taxa.py
@@ -3,7 +3,7 @@ def valor_score_serasa(cnpj: str) -> int:
     from requests import post, get
     pload = {"cnpj": cnpj}
     try:
-        req = post("http://localhost:8082/consultas/v1/score", json = pload)
+        req = post("http://localhost:8082/consultas/v1/score", json=pload)
         req = json.loads(req.content)
         if req["ok"] and req["status"] == 200:
             return req["data"]["score"]
@@ -15,66 +15,32 @@ def valor_score_serasa(cnpj: str) -> int:
         return randrange(0, 1000)
 
 
-def calcular_taxas (
+def calcular_taxas(
         renda_mensal: float,
         patrimonio: float,
         idade_usuario: int,
         tempo_financiamento: int,
         valor_imovel: float,
-        score_serasa: int,
-        taxa_administracao = 0.01,
-        seguro_dfi = 0.00015,
-        seguro_mip = 0.0002
-    ) -> list:
-    # renda
-    renda = renda_mensal
-    p_renda = -750
-    # patrimonio
-    patri = patrimonio
-    p_patri = -100
-    # idade
-    idade = idade_usuario
-    p_idade = 10_000_000
-    # tempo de financiamento
-    tf = tempo_financiamento
-    p_tf = 50_000
-    # valor do imovel
-    vi = valor_imovel
-    p_vi = 350
-    # score do serasa
-    scs = score_serasa
-    p_scs = -5_000
+        score_serasa: int
+) -> dict:
+    from taxa_financiamento import TaxaFinanciamento
 
-    # taxa de administraçao
-    ta = taxa_administracao # 1% de taxa
-    p_ta = 1_000
-    # taxa de seguro dfi
-    dfi = seguro_dfi # 0.015% de seguro dfi
-    p_dfi = 200
-    # taxa de seguro mip
-    mip = seguro_mip # 0.02% de seguro mip
-    p_mip = 200
+    financ = TaxaFinanciamento(renda_mensal, patrimonio, idade_usuario, tempo_financiamento, valor_imovel, score_serasa)
+    taxa = financ.calc_taxa_total()
 
-    y_taxa = (
-        ((ta * p_ta) + (dfi * p_dfi) + (mip * p_mip))
-        /
-        ((p_ta + p_dfi + p_mip))
-    ) * 6800
+    taxa_administracao = 1
+    seguro_dfi = 0.018
+    seguro_mip = financ.calc_seguro_mip()
 
-    x_taxa = (
-        ((renda * p_renda) + (patri * p_patri) + (idade * p_idade) + (tf * p_tf) + (vi * p_vi) + (scs * p_scs))
-        /
-        (p_renda + p_patri + p_idade + p_tf + p_vi + p_scs)
-    )
+    y_taxa = (taxa_administracao + seguro_dfi + seguro_mip)
 
-    taxa = (x_taxa * 0.3) + y_taxa
-    taxa_sem_extra = (taxa / 10) - 1.035
-    
+    x_taxa = taxa - y_taxa
+
     taxas = {
-        "taxa": round(taxa_sem_extra / 100, 3),
-        "taxa_administracao": taxa_administracao,
-        "dfi": seguro_dfi,
-        "mip": seguro_mip,
-        "taxa_total": round(((taxa / 10) / 100), 3)
+        "taxa": round((x_taxa / 100), 3),
+        "taxa_administracao": round((taxa_administracao / 100), 2),
+        "dfi": round((seguro_dfi / 100), 6),
+        "mip": round((seguro_mip / 100), 6),
+        "taxa_total": round((taxa / 100), 3)
     }
     return taxas
--- apis-externas/s16-bank-api/main.py
+++ apis-externas/s16-bank-api/main.py
@@ -1,8 +1,10 @@
 import sqlite3
 from flask import Flask, request, g, jsonify, make_response
 from markupsafe import escape
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+
 
 @app.errorhandler(500)
 def not_found(error):
@@ -13,6 +15,7 @@ def not_found(error):
         "status": 500
     }), 500)
 
+
 @app.errorhandler(405)
 def not_found(error):
     # return make_response("Method not Allowed", 405)
@@ -20,6 +23,7 @@ def not_found(error):
         "ok": False,
         "status": 405
     }), 405)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -30,6 +34,7 @@ def not_found(error):
         "status": 404
     }), 404)
 
+
 @app.errorhandler(400)
 def not_found(error):
     # return make_response("Bad Request", 400)
@@ -39,7 +44,9 @@ def not_found(error):
         "status": 400
     }), 400)
 
+
 DATABASE = 's16bank.db'
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -47,9 +54,11 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
+
 def create_db():
     from values_db import insert
     insert()
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -57,15 +66,17 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
 def select_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+
 @app.route('/openbanking/v1/financiamento', methods=['POST'])
 def financiamento():
-    #create_db()
+    # create_db()
     body = request.json
     cnpj = body['cnpj']
     renda = body['renda']
@@ -122,9 +133,10 @@ def financiamento():
     }
     return make_response(jsonify(resposta), 200)
 
+
 @app.route('/openbanking/v1/conta', methods=['POST'])
 def conta():
-    #create_db()
+    # create_db()
     body = request.json
     cnpj = body['cnpj']
 
