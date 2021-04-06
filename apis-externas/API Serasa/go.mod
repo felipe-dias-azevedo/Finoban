18e815028bacd130398e5eb6eec8d459fe9b4b08
jusebandtec <jose.silva@bandtec.com.br>
2021-04-05 23:28:12 -0300

    Finalizando API Serasa

--- apis-externas/API Serasa/go.mod
+++ apis-externas/API Serasa/go.mod
@@ -2,4 +2,4 @@ module main.go
 
 go 1.16
 
-require github.com/mattn/go-sqlite3 v1.14.6 // indirect
+require github.com/mattn/go-sqlite3 v1.14.6
--- apis-externas/API Serasa/main.go
+++ apis-externas/API Serasa/main.go
@@ -6,40 +6,82 @@ import (
 	"log"
 	"net/http"
 
-	_ "github.com/mattn/go-sqlite3"
+	"encoding/json"
+	_ "encoding/json"
+
+	_ "github.com/mattn/go-sqlite3" // Add This
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Homepage Endpoint Hit")
+	// fmt.Fprint(w, "Homepage Endpoint Hit")
+	var user User
+	var response Response
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response.Ok = true
+	response.Status = 200
+	response.Data.Score = selectBancoDeDados(user.CNPJ).Score
+	w.WriteHeader(200)
+	jsonResponse, err := json.Marshal(response)
+	w.Write(jsonResponse)
 }
 
 func handleRequests() {
 	http.HandleFunc("/consultas/v1/score", homePage)
 	log.Fatal(http.ListenAndServe(":8082", nil))
+
 }
 
 func main() {
 	handleRequests()
-	criarBancoDados()
+
 }
 
-func criarBancoDados() {
-	database, _ := sql.Open("sqlite3", "./serada.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS Cliente (CNPJ INTEGER PRIMARY KEY, nome TEXT, patrimonio REAL, date DATE")
-	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO Cliente values (12345678900, 'Carlos Roberto', 50000.0, 1985-12-16), (12345, 'João da Silva', 10000.0, 1993-05-24), (123, 'Silvio Santos',  1000000.0, 1970-01-01), (456, 'Gabriel Rodrigues',  50000.0, 1995-03-27), (789, 'Fernando Brandão',  10000.0, 1998-06-14);")
-	statement.Exec()
+func selectBancoDeDados(cnpjCliente int) Usuario {
+	database, _ := sql.Open("sqlite3", "./serasa.db")
 	rows, _ := database.Query("SELECT * FROM Cliente")
-	var cnpj string
+	var usuario Usuario
+	var cnpj int
 	var nome string
 	var patrimonio float64
 	var data string
+	var score int
 	for rows.Next() {
-		rows.Scan(&cnpj, &nome, &patrimonio, &data)
-		fmt.Println(cnpj, ":", nome, ", ", patrimonio, ", ", data)
+		rows.Scan(&cnpj, &nome, &patrimonio, &data, &score)
+		if cnpj == cnpjCliente {
+			fmt.Println(cnpj, ":", nome, ", ", patrimonio, ", ", data, ", ", score)
+			usuario.CNPJ = cnpj
+			usuario.Nome = nome
+			usuario.Patrimonio = patrimonio
+			usuario.Data = data
+			usuario.Score = score
+			return usuario
+		}
 	}
+	return usuario
+}
+
+type Usuario struct {
+	CNPJ       int
+	Nome       string
+	Patrimonio float64
+	Data       string
+	Score      int
 }
 
 type User struct {
-	CNPJ string
+	CNPJ int
+}
+
+type Response struct {
+	Ok     bool
+	Status int
+	Data   Data
+}
+type Data struct {
+	Score int
 }
