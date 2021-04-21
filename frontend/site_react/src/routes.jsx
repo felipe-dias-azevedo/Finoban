61697367ed6d0c58f62f243b19ec4b1b440654eb
mario-heleno <mario.silvafilho@bandtec.com.br>
2021-04-21 20:37:28 -0300

    Página dashboard

--- frontend/site_react/src/routes.jsx
+++ frontend/site_react/src/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
 import PaginaInicial from './pages/PaginaInicial';
 import Simulador from './pages/Simulador';
 
@@ -9,6 +10,7 @@ function Routes() {
             <Switch>
                 <Route exact path="/" component={PaginaInicial} />
                 <Route exact path="/simulador" component={Simulador} />
+                <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="*" component={PaginaInicial} />
             </Switch>
         </Router>
