e2cfbcd4048466d20ef7f46fab0041c04f3388f3
mario-heleno <mario.silvafilho@bandtec.com.br>
2021-06-07 20:43:26 -0300

    Implementando classe fila

--- api-finoban/finoban/src/main/java/com/bandtec/br/finoban/repository/AvaliacaoRepository.java
+++ api-finoban/finoban/src/main/java/com/bandtec/br/finoban/repository/AvaliacaoRepository.java
@@ -1,9 +1,11 @@
 package com.bandtec.br.finoban.repository;
 
+import com.bandtec.br.finoban.entidades.Acesso;
 import com.bandtec.br.finoban.entidades.Avaliacao;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AvaliacaoRepository extends CrudRepository <Avaliacao, Integer> {
-
-
+    List<Avaliacao> findByFkAcessoEquals(Acesso fkAcesso);
 }
