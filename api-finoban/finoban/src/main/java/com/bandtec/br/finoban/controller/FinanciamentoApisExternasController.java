7dd30b877b9d4069867dbcb249f0d55bbb289a8d
José Silva <jose.silva@bandtec.com.br>
2021-06-14 21:30:30 -0300

    mudando ips do controller que faz requisição na instancia

--- api-finoban/finoban/src/main/java/com/bandtec/br/finoban/controller/FinanciamentoApisExternasController.java
+++ api-finoban/finoban/src/main/java/com/bandtec/br/finoban/controller/FinanciamentoApisExternasController.java
@@ -51,9 +51,9 @@ public class FinanciamentoApisExternasController {
         List<String> listUrl = new ArrayList<>();
         List<RespostaApi> listResponse = new ArrayList<>();
 
-        listUrl.add("http://18.207.233.50:3333/openbanking/v1/financiamento");
-        listUrl.add("http://18.207.233.50:5000/openbanking/v1/financiamento");
-        listUrl.add("http://18.207.233.50:8000/openbanking/v1/financiamento");
+        listUrl.add("http://54.226.124.160:3333/openbanking/v1/financiamento");
+        listUrl.add("http://54.226.124.160:5000/openbanking/v1/financiamento");
+        listUrl.add("http://54.226.124.160:8000/openbanking/v1/financiamento");
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
