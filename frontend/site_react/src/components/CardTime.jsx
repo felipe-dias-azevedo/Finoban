d866257d3b01976d55da8189195e8c065c7f0c13
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-09-12 19:57:22 -0300

    alteração do css sobre nós

--- frontend/site_react/src/components/CardTime.jsx
+++ frontend/site_react/src/components/CardTime.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import Jose from '../assets/images/ze.png'
 
-function CardTime(props){
-return(
-<>
+function CardTime(props) {
+    return (
+        <>
+            <div className="container">
+                <div className="box">
 
-        <div className="box">
-                <div className="boximg">
-              
+                    <div className="teste">
+                        <img src="{Jose}" alt="" className="img" />
+                    </div>
+
+
+                    <div class="name">
+                        <label>
+                            <h4>{props.nome}</h4>
+                        </label>
+                    </div>
                 </div>
-            
-                <div className="name">
-                    <label>
-                    <h4 className="card">{props.nome}</h4>
-                    </label>
-        
-                </div>
+
             </div>
-      
-</>
-);
+
+
+
+
+
+
+        </>
+    );
 
 }
 export default CardTime;
--- frontend/site_react/src/pages/SobreNos.jsx
+++ frontend/site_react/src/pages/SobreNos.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import CardTime from "../components/CardTime";
+import Jose from '../assets/images/ze.png';
+import Cat from '../assets/images/cat.png';
+import Mario from '../assets/images/mario.png';
+import Victor from '../assets/images/victor.png';
+import Vini from '../assets/images/vini.png';
+import Felipe from '../assets/images/felipe.png';
+
+
 
 
 function SobreNos() {
@@ -10,65 +18,55 @@ function SobreNos() {
         <>
             <Header />
 
-            <div className="container">
-
-                <h3 className="h33">Sobre o Finoban</h3>
-
-                <div>
-
-                    O Finoban é um projeto pensado e desenvolvido por alunos da faculdade Bandtec. Com o objetivo de
-                    entregar uma solução tecnológica para o mercado financeiro, focada no ramo do financiamento
-                    imobiliário.
-
-                </div>
 
 
+            <div class="sobrenos">
 
-            </div>
+                <div class="boxNormal">
+
+                    <div className="titulo">
+                        <h3>Sobre o Finoban</h3>
+                    </div>
+
+                    <div className="conteudo">
+                        <p>O Finoban é um projeto pensado e desenvolvido por alunos da
+                            faculdade Bandtec. Com o objetivo de
+                            entregar
+                            uma solução tecnológica para o mercado
+                            financeiro, focada no ramo do financiamento
+                            imobiliário.</p>
+                    </div>
 
 
-
-
-            <div className="boxes">
-
-
-                <div className="FirstBox">
-
-                    <CardTime
-                        nome="Vinicius Carvalho"
-                    />
-
-                    <CardTime
-
-                        nome="José Paulo"
-                    />
-                    <CardTime
-
-                        nome="Catarina Carneiro"
-                    />
-
-                </div>
-
-                <div className="SecundBox">
-                    <CardTime
-
-                        nome="Felipe Dias"
-                    />
-                    <CardTime
-
-                        nome="Mario Heleno"
-                    />
-                    <CardTime
-
-                        nome="Victor Barbosa"
-                    />
 
                 </div>
 
 
 
 
-            </div>
+                <div className="boxes">
+
+                    <div className="box">
+
+                        <div className="teste">
+                            <img className="img" src="../assets/images/ze.png" alt="" />
+                        </div>
+
+                        <div className="name">
+                            <label>
+                                <h4>Jose Paulo</h4>
+                            </label>
+                        </div>
+
+                    </div>
+
+                    <CardTime
+                        nome="jossssssssssssssssss" />
+
+
+                </div>  </div>
+
+
 
             <Footer />
         </>
