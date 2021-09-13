43692ccf6b4673ba15b6c56c32bd09b3d817b3f2
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-09-12 22:45:59 -0300

    react tela sobre nós cards, emnum e componet card time

--- frontend/site_react/src/utils/timeEnum.js
+++ frontend/site_react/src/utils/timeEnum.js
@@ -3,7 +3,7 @@ import Mario from '../assets/images/mario.png';
 import Victor from '../assets/images/victor.png';
 import Vini from '../assets/images/vini.png';
 import Felipe from '../assets/images/felipe.png';
-
+import Jose from '../assets/images/ze.png';
 
 const timeEnum = {
     CATARINA: {
@@ -19,12 +19,12 @@ const timeEnum = {
     JOSE: {
         name: "José Marinho",
         imageSource: Jose,
-        assign: "Dev. Fullstack"
+        assign: "Dev. Back-End"
     },
     VINICIUS: {
         name: "Vinícius Carvalho",
         imageSource: Vini,
-        assign: "Dev. Fullstack"
+        assign: "Design/UX/UI"
     },
     VICTOR: {
         name: "Victor Barbosa",
@@ -34,7 +34,7 @@ const timeEnum = {
     MARIO: {
         name: "Mário Heleno",
         imageSource: Mario,
-        assign: "Dev. Fullstack"
+        assign: "DBA"
     }
 }
 
