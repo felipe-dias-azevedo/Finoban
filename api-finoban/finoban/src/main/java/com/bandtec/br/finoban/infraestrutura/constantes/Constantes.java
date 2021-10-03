a90e11f52084c0dc2cbc218a6a40b2ed3fb5788a
jusebandtec <jose.silva@bandtec.com.br>
2021-10-03 00:38:19 -0300

    removendo uma linha a mais no construtor da ResponseLogin e no loginService, removendo recursoesHelper

--- api-finoban/finoban/src/main/java/com/bandtec/br/finoban/infraestrutura/constantes/Constantes.java
+++ api-finoban/finoban/src/main/java/com/bandtec/br/finoban/infraestrutura/constantes/Constantes.java
@@ -2,5 +2,5 @@ package com.bandtec.br.finoban.infraestrutura.constantes;
 
 public class Constantes {
     public static final String SALT_FINOBAN  = "_FINOBAN_BEST";
-    public static final String URL_REDEFINIR_SENHA = "http://localhost:3000/usuarios/redefinir-senha/";
+    public static final String URL_REDEFINIR_SENHA = "http://localhost:3000/esqueci-minha-senha/";
 }
