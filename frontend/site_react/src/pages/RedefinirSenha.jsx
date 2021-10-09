cff2b2b6ce6d02a43915d5ac710bb3ad5e8da1b2
jusebandtec <jose.silva@bandtec.com.br>
2021-10-08 21:32:01 -0300

    add: adicionando funções à página de redefinição de senha

--- frontend/site_react/src/pages/RedefinirSenha.jsx
+++ frontend/site_react/src/pages/RedefinirSenha.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useHistory } from "react-router";
+import { useEffect } from "react"
+import { useHistory, useParams } from "react-router";
 import api from "../services/api";
 import { Link } from "react-router-dom";
 import LogoFinobanLight from "../assets/images/logo-finoban-light.svg";
@@ -13,7 +14,55 @@ import { toast } from "react-toastify";
 import UseForm from "../components/UseForm";
 import validate from "../components/ValidacaoFormEsqueciSenha";
 
-export default function RedefinirSenha() {
+const Form = () => {
+
+	const history = useHistory();
+	const { jwt } = useParams();
+	const [jwtIsValid, setJwtIsValid] = useState(false);
+	const [emailRetornoRequisicao, setEmailRetornoRequisicao] = useState("");
+	const [novaSenha, setNovaSenha] = useState("");
+	const [confirmacaoNovaSenha, setConfirmacaoNovaSenha] = useState("");
+
+	useEffect(() => {
+		api.get(`/usuarios/redefinir-senha/verificar/${jwt}`).then((e) => {
+			if (e.status == 200) {
+				setJwtIsValid(true);
+				setEmailRetornoRequisicao(e.data.data.email);
+				console.log(e.data.data)
+			}
+		})
+			.catch((e) => {
+				const status = e.response.data.code;
+				if (status == "FIN13") toast.error("Token Expirado");
+			});
+	}, [])
+
+
+	const RedefinirSenha = (e) => {
+
+		if (!jwtIsValid) toast.error("Token inválido")
+
+		e.preventDefault();
+		
+		if (novaSenha != confirmacaoNovaSenha) toast.error("Senhas diferentes")
+
+
+		const data = {
+			email: emailRetornoRequisicao,
+			tokenJwt: jwt,
+			novaSenha: novaSenha
+		}
+		
+		api.post('/usuarios/redefinir-senha', data).then((e) => {
+			if (e.status == 200) {
+				toast.success("Senha alterada com sucesso");
+				history.push({
+					pathname: "/login",
+				});
+			}
+		})
+	}
+
 	return (
 		<main>
 			<div className="container">
@@ -21,31 +70,46 @@ export default function RedefinirSenha() {
 					<img src={LogoFinobanLight} alt="" />
 				</div>
 				<div className="content-form">
-					<form action="">
+					<form action="" onSubmit={RedefinirSenha}>
 						<br />
 						<h1 className="text-center">Redefinição de senha</h1>
 						<br />
 						<div className="redefinir-senha">
 							<h4>Insira sua senha:</h4>
-							<input type="password" />
+							<input
+								type="password"
+								onChange={(e) => setNovaSenha(e.target.value)}
+							/>
 							<h4 className="mt-4">Repita sua senha:</h4>
-							<input type="password" />
+							<input
+								type="password"
+								onChange={(e) => setConfirmacaoNovaSenha(e.target.value)}
+							/>
 
-                            <div className="d-flex flex-row justify-content-beetwen mt-5 fonte-15">
-							<button
-								type="submit"
-								className="bg-transparente cor-verde mr-15"
-							>
-								<Link to="/login">Voltar ao login</Link>
-							</button>
-							<button type="submit">
-								Redefinir senha
-							</button>
-                            </div>
+							<div className="d-flex flex-row justify-content-beetwen mt-5 fonte-15">
+								<button
+									type="submit"
+									className="bg-transparente cor-verde mr-15"
+								>
+									<Link to="/login">Voltar ao login</Link>
+								</button>
+								<button type="submit">
+									Redefinir senha
+								</button>
+							</div>
 						</div>
 					</form>
 				</div>
 			</div>
 		</main>
 	);
+}
+
+export default function RedefinirSenha() {
+	return (
+		<>
+			<ModalAviso />
+			<Form />
+		</>
+	)
 }
