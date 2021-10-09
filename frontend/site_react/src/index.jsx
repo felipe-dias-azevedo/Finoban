c1f8a97caf4b6446263b03f5ec1486392574eba1
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2021-10-08 23:27:40 -0300

    fix: duplicating page bug

--- frontend/site_react/src/index.jsx
+++ frontend/site_react/src/index.jsx
@@ -4,12 +4,21 @@ import App from './App';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { DarkModeProvider } from './contexts/DarkModeContext';
+import { UserProvider } from './contexts/UserContext';
+import { NegocioProvider } from './contexts/NegocioContext';
+import { SwitchGraficoNegocioProvider } from './contexts/SwitchGraficoNegocioContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <DndProvider backend={HTML5Backend}>
       <DarkModeProvider>
-        <App />
+        <UserProvider>
+          <NegocioProvider>
+            <SwitchGraficoNegocioProvider>
+              <App />
+            </SwitchGraficoNegocioProvider>
+          </NegocioProvider>
+        </UserProvider>
       </DarkModeProvider>
     </DndProvider>
   </React.StrictMode>,
--- frontend/site_react/src/routes.jsx
+++ frontend/site_react/src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Cadastro from './pages/Cadastro';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -12,37 +12,29 @@ import GraficoNegocio from './pages/AnaliseNegocio/GraficoNegocio';
 import SobreNos from './pages/SobreNos';
 import EsqueciMinhaSenha from './pages/EsqueciMinhaSenha';
 import RedefinirSenha from './pages/RedefinirSenha';
-import { NegocioProvider } from './contexts/NegocioContext';
-import { UserProvider } from './contexts/UserContext';
-import { SwitchGraficoNegocioProvider } from './contexts/SwitchGraficoNegocioContext';
 import PaginaNaoExiste from './pages/PaginaNaoExiste';
 
 function Routes() {
-
     return (
         <Router>
             <Switch>
-                <UserProvider>
-                    <Route exact path="/" component={PaginaInicial} />
-                    <Route exact path="/simulador" component={Simulador} />
-                    <Route exact path="/dashboard" component={Dashboard} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/cadastro" component={Cadastro} />
-                    <Route exact path="/openbanking" component={Openbanking} />
-                    <Route exact path="/sobrenos" component={SobreNos} />
-                    <Route exact path="/analise" component={LoginNegocio} />
-                    <Route exact path="/esqueci-minha-senha" component={EsqueciMinhaSenha} />
-                    <Route exact path="/esqueci-minha-senha/:jwt" component={RedefinirSenha} />
+                <Route exact path="/" component={PaginaInicial}/>
+                <Route exact path="/simulador" component={Simulador} />
+                <Route exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/cadastro" component={Cadastro} />
+                <Route exact path="/openbanking" component={Openbanking} />
+                <Route exact path="/sobrenos" component={SobreNos} />
 
-                    <NegocioProvider>
-                        <SwitchGraficoNegocioProvider>
-                            <Route exact path="/analise/dashboard" component={DashboardNegocio} />
-                        </SwitchGraficoNegocioProvider>
-                        <Route exact path="/analise/dashboard/:id" component={GraficoNegocio} />
-                    </NegocioProvider>
-
-                    <Route path="*" component={PaginaNaoExiste} />
-                </UserProvider>
+                <Route exact path="/esqueci-minha-senha" component={EsqueciMinhaSenha} />
+                <Route exact path="/esqueci-minha-senha/:jwt" component={RedefinirSenha} />
+                    
+                <Route exact path="/analise" component={LoginNegocio} />
+                <Route exact path="/analise/dashboard" component={DashboardNegocio} />
+                <Route exact path="/analise/dashboard/:id" component={GraficoNegocio} />
+                                    
+                <Route exact path="/not-found" component={PaginaNaoExiste} />
+                <Redirect from="*" to="/not-found" />
             </Switch>
         </Router>
     );
