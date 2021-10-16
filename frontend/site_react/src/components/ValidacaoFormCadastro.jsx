a58e402f49c7575bd80adce4d9375dfd18358a38
Victor Barbosa <victor.barbosa@bandtec.com.br>
2021-10-15 21:34:03 -0300

    add: validacao de cpf

--- frontend/site_react/src/components/ValidacaoFormCadastro.jsx
+++ frontend/site_react/src/components/ValidacaoFormCadastro.jsx
@@ -1,68 +1,44 @@
-function validarCNPJ(cnpj) {
-	cnpj = cnpj.replace(/[^\d]+/g, "");
-
-	if (cnpj === "") return false;
-
-	if (cnpj.length != 14) return false;
-
-	// Elimina CNPJs invalidos conhecidos
-	if (
-		cnpj === "00000000000000" ||
-		cnpj === "11111111111111" ||
-		cnpj === "22222222222222" ||
-		cnpj === "33333333333333" ||
-		cnpj === "44444444444444" ||
-		cnpj === "55555555555555" ||
-		cnpj === "66666666666666" ||
-		cnpj === "77777777777777" ||
-		cnpj === "88888888888888" ||
-		cnpj === "99999999999999"
-	)
+function validarCpf(cpf) {
+	cpf = cpf.replace(/\D/g, "");
+	if (cpf.toString().length != 11 || /^(\d)\1{10}$/.test(cpf)) {
 		return false;
-
-	// Valida DVs
-	var tamanho, numeros, digitos, soma, pos, resultado;
-	tamanho = cnpj.length - 2;
-	numeros = cnpj.substring(0, tamanho);
-	digitos = cnpj.substring(tamanho);
-	soma = 0;
-	pos = tamanho - 7;
-	for (var i = tamanho; i >= 1; i--) {
-		soma += numeros.charAt(tamanho - i) * pos--;
-		if (pos < 2) pos = 9;
 	}
-	resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-	if (resultado != digitos.charAt(0)) return false;
 
-	tamanho = tamanho + 1;
-	numeros = cnpj.substring(0, tamanho);
-	soma = 0;
-	pos = tamanho - 7;
-	for (var i = tamanho; i >= 1; i--) {
-		soma += numeros.charAt(tamanho - i) * pos--;
-		if (pos < 2) pos = 9;
-	}
-	resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-	if (resultado != digitos.charAt(1)) return false;
+	var resultado = true;
 
-	return true;
+	[9, 10].forEach(function (j) {
+		var soma = 0,
+			r;
+		cpf.split(/(?=)/)
+			.splice(0, j)
+			.forEach(function (e, i) {
+				soma += parseInt(e) * (j + 2 - (i + 1));
+			});
+		r = soma % 11;
+		r = r < 2 ? 0 : 11 - r;
+		if (r != cpf.substring(j, j + 1)) resultado = false;
+	});
+
+	return resultado;
 }
 
 export default function validate(values) {
+	console.log(values);
 	let errors = {};
 	if (!values.nome) {
 		errors.nome = "Insira seu nome completo";
-	} if (typeof values.nome != "string") {
+	}
+	if (typeof values.nome != "string") {
 		errors.nome = "Insira um nome válido";
 	} else if (typeof values.nome == "string" && values.nome.length < 5) {
 		errors.nome = "Insira um nome válido";
 	}
 
-	if (!values.cnpj) {
-		errors.cnpj = "Insira um CNPJ";
-	} else if (values.cnpj) {
-		if (!validarCNPJ(values.cnpj)) {
-			errors.cnpj = "CNPJ inválido!";
+	if (!values.cpf) {
+		errors.cpf = "Insira um CPF";
+	} else if (values.cpf) {
+		if (!validarCpf(values.cpf)) {
+			errors.cpf = "Insira um CPF válido";
 		}
 	}
 
