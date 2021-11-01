ae01a61cf53ccfa674703b1fa9d0553631c7051f
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-11-01 01:10:09 -0300

    configurando ambiente e inicando testes em Selenium

--- api-finoban/finoban/src/test/java/com/bandtec/br/finoban/selenium/Selenium.java
+++ api-finoban/finoban/src/test/java/com/bandtec/br/finoban/selenium/Selenium.java
@@ -1,2 +1,18 @@
-package com.bandtec.br.finoban;public class Selenium {
+package com.bandtec.br.finoban.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+
+public class Selenium {
+
+    @Test
+    public void Teste1DeFuncionamento(){
+
+        System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://localhost:8080/finoban");
+        browser.quit();
+
+    }
 }
