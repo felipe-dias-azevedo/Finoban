a969406c8daa7cbcf160466780324e819f25cc17
Victor Barbosa <victor.barbosa@bandtec.com.br>
2021-11-01 22:54:48 -0300

    fix: layout testes-projetos

--- frontend/site_react/src/components/AnaliseTeste/TesteProjetoCard.jsx
+++ frontend/site_react/src/components/AnaliseTeste/TesteProjetoCard.jsx
@@ -1,50 +1,66 @@
 import React, { useContext } from "react";
 import { DarkModeContext } from "../../contexts/DarkModeContext";
-import Chart from 'react-google-charts';
+import Chart from "react-google-charts";
 
 function BankCard(props) {
+	console.log(props);
 
-    const { isDarkEnable } = useContext(DarkModeContext);
-    const chartBackgroundColor = isDarkEnable ? '#353535' : '#ffffff';
-    const btnBackgroundColor = props.porcentagem_sucesso >= 80 ? '#64AE6C' : '#DA5C5C';
+	const { isDarkEnable } = useContext(DarkModeContext);
+	const chartBackgroundColor = isDarkEnable ? "#353535" : "#ffffff";
+	const btnBackgroundColor = props.estatus ? "#64AE6C" : "#DA5C5C";
 
-    const dataChart = [
-        ['Status', 'Porcentagem'],
-        ['Passou', (props.porcentagem_sucesso * 100)],
-        ['Falhou', 100 - (props.porcentagem_sucesso * 100)]
-    ]
+	const dataChart = [
+		["Status", "Porcentagem"],
+		["Passou", props.porcentagem_sucesso * 100],
+		["Falhou", 100 - props.porcentagem_sucesso * 100],
+	];
 
-    return (
-        <>
-            <div className="box-card-teste shadow">
-                <span className="nome-projeto-card-teste"> {props.nome_aplicacao} </span>
-                <div className="btn-card-teste" style={{ backgroundColor: btnBackgroundColor }}> {props.estatus ? 'Passou' : 'Falhou'} </div>
-                <span className="qtd-test-card-teste"> {props.qtd_testes} </span>
-                <span className="data-card-teste"> {props.data_execucao} </span>
-                <span className="duracao-card-teste"> {props.duracao_execucao} </span>
-                <div className="grafico-torta-porcentagem">
-                    <Chart
-                        width={'100px'}
-                        height={'100px'}
-                        chartType="PieChart"
-                        loader={<div>Carregando o gráfico</div>}
-                        data={dataChart}
-                        options={{
-                            tooltip: { textStyle: { fontSize: 10 } },
-                            legend: 'none',
-                            pieSliceText: 'none',
-                            pieHole: 0.7,
-                            slices: {
-                                0: { color: '#64AE6C' },
-                                1: { color: '#DA5C5C' },
-                            },
-                            backgroundColor: chartBackgroundColor,
-                        }}
-                    />
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="box-card-teste shadow">
+				<span className="nome-projeto-card-teste">
+					{" "}
+					{props.nome_aplicacao}{" "}
+				</span>
+				<div
+					className="btn-card-teste"
+					style={{ backgroundColor: btnBackgroundColor }}
+				>
+					{" "}
+					{props.estatus ? "Passou" : "Falhou"}{" "}
+				</div>
+				<span className="qtd-test-card-teste">
+					{" "}
+					{props.qtd_testes}{" "}
+				</span>
+				<span className="data-card-teste"> {props.data_execucao} </span>
+				<span className="duracao-card-teste">
+					{" "}
+					{props.duracao_execucao}s{" "}
+				</span>
+				<div className="grafico-torta-porcentagem">
+					<Chart
+						width={"100px"}
+						height={"100px"}
+						chartType="PieChart"
+						loader={<div>Carregando o gráfico</div>}
+						data={dataChart}
+						options={{
+							tooltip: { textStyle: { fontSize: 1 } },
+							legend: "none",
+							pieSliceText: "none",
+							pieHole: 0.7,
+							slices: {
+								0: { color: "#64AE6C" },
+								1: { color: "#DA5C5C" },
+							},
+							backgroundColor: chartBackgroundColor,
+						}}
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default BankCard;
--- frontend/site_react/src/pages/AnaliseTestes/TestesProjetos.jsx
+++ frontend/site_react/src/pages/AnaliseTestes/TestesProjetos.jsx
@@ -2,23 +2,24 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import TesteProjetoCard from "../../components/AnaliseTeste/TesteProjetoCard";
 import api from "../../services/api";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function TestesProjetos() {
-
 	const [responseProjectTest, setResponseProjectTests] = useState([]);
 
 	const getTestes = async () => {
 		try {
 			const response = await api.get("/tests/apps");
 			console.log(response);
-			setResponseProjectTests(response.data[0])
+			setResponseProjectTests(response.data);
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
-	getTestes();
+	useEffect(() => {
+		getTestes();
+	}, []);
 
 	return (
 		<>
@@ -29,20 +30,20 @@ function TestesProjetos() {
 				<h5> Estatus </h5>
 				<h5> Testes </h5>
 				<h5> Data execução </h5>
-				<h5> Duração execução (s) </h5>
+				<h5> Duração execução </h5>
 				<h5> % de sucesso </h5>
 			</div>
 
-			{responseProjectTest.map((project) => {
+			{responseProjectTest.map((project) => (
 				<TesteProjetoCard
 					nome_aplicacao={project.nomeAplicacao}
-					estatus={project.estatus == 'Passou' ? true : false}
+					estatus={project.statusGeral == "PASSOU" ? true : false}
 					qtd_testes={project.quantidadeTestes}
 					data_execucao={project.dataExecucao}
 					duracao_execucao={project.duracaoExecucao}
 					porcentagem_sucesso={project.porcentagemSucesso}
 				/>
-			})}
+			))}
 
 			<Footer />
 		</>
