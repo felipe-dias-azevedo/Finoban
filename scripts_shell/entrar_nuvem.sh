af8d7da4c031e043a9f9c6a686723ca584332042
Victor Barbosa <victor.barbosa@bandtec.com.br>
2021-11-25 19:21:03 -0300

    update: scripts para acessar a instancia

--- scripts_shell/entrar_nuvem.sh
+++ scripts_shell/entrar_nuvem.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #ssh -i "Chave_Finoban_AWS.pem" ubuntu@ec2-54-226-124-160.compute-1.amazonaws.com
-ssh -i "chave-aws-ec2-finoban-2021-2.pem" ubuntu@ec2-3-222-7-102.compute-1.amazonaws.com
+ssh -i "grupo-finoban.pem" ubuntu@3.229.234.24
