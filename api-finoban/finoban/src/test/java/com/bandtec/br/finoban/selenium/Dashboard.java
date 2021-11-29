605a4e7f7c2f2f3ddfc54412f9981b24ee02b499
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-11-29 11:55:52 -0300

    finalizando alguns pontos do teste rodar em prod

--- api-finoban/finoban/src/test/java/com/bandtec/br/finoban/selenium/Dashboard.java
+++ api-finoban/finoban/src/test/java/com/bandtec/br/finoban/selenium/Dashboard.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Dashboard {
 
     @Test
-    public void Usuário_fez_uma_simulação_banco_S16bank_deve_ir_para_Dashboard() throws InterruptedException {
+    public void Usuário_fez_uma_simulação_completa_() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "src/drivers/chromedriver.exe");
         WebDriver browser = new ChromeDriver();
         browser.navigate().to("http://52.6.99.67/");
@@ -22,28 +24,30 @@ public class Dashboard {
 
         browser.findElement(By.id("bt-simular")).click();
 
-        Thread.sleep(10000);
+        Thread.sleep(7000);
 
-        browser.findElement(By.id("btn-bank-card")).click();
-
-        Thread.sleep(5000);
+        browser.findElement(By.className("btn-bank-card")).click();
 
         browser.findElement(By.id("bnt-hub-login")).click();
+        Thread.sleep(1000);
 
-        browser.findElement(By.id("email")).sendKeys("felipe@admin.com");
+        browser.findElement(By.id("email")).sendKeys("catarinacarneiro@bandtec.com.br");
         browser.findElement(By.id("password")).sendKeys("123456");
         browser.findElement(By.id("submit")).click();
 
-        Thread.sleep(10000);
+        Thread.sleep(7000);
+        browser.findElement(By.className("btn-contratar")).click();
+
+        Thread.sleep(5000);
+        browser.findElement(By.id("wpp")).click();
+
+        Thread.sleep(5000);
+        assertTrue(browser.getCurrentUrl().equals("https://api.whatsapp.com/send/?phone=551131758248&text&app_absent=0"));
+        assertTrue(browser.getPageSource().contains("Banco Safra"));
 
         browser.quit();
-
-        
-
-
-
-
     }
+
 
 
 }
